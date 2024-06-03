c25fe1569699094f037ff0e383998a0ebd7bf9f7
234992 <90908153+234992@users.noreply.github.com>
2024-06-03 15:29:14 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Silver/9655. 돌 게임/돌 게임.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main()
+{
+   //상근이가 게임을 이기면 SK를, 창영이가 게임을 이기면 CY을 출력한다.
+    int a;
+    scanf("%d",&a);
+    if (a % 2 == 0)
+        printf("CY");
+    else
+        printf("SK");
+   
+    return 0;
+}
+
+
+
+
