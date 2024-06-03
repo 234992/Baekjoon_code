dc215563c69c9f3c45dead0d680f07c5e37c3e8f
234992 <90908153+234992@users.noreply.github.com>
2024-06-03 15:38:38 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main()
+{
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |");
+
+
+    return 0;
+}
+
