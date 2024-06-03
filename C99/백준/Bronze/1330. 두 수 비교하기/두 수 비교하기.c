377e818826203fb2a7cac33b2b852635a0f0a97b
234992 <90908153+234992@users.noreply.github.com>
2024-06-03 15:31:45 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,20 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a == b)
+    {
+        printf("==");
+    }
+    if (a < b)
+    {
+        printf("<");
+    }
+    if (a > b)
+    {
+        printf(">");
+    }
+    return 0;
+}
