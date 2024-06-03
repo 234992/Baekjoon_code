cd5cc06751e952bdf7a613e06d4f3a480e201263
234992 <90908153+234992@users.noreply.github.com>
2024-06-03 15:35:29 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/10172. 개/개.c
@@ -0,0 +1,12 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main()
+{
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+    return 0;
+}
+
