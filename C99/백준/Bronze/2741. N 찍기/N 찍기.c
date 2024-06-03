107e90fa1f8c595da2200e9e9a57db6c2aa43e62
234992 <90908153+234992@users.noreply.github.com>
2024-06-03 15:32:37 +0900

    [Bronze V] Title: N 찍기, Time: 16 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/2741. N 찍기/N 찍기.c
@@ -0,0 +1,16 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main()
+{
+    int a;;
+
+    scanf("%d", &a);
+    for (int i=1; i < a+1; ++i)
+    {
+        printf("%d\n", i);
+    }
+
+
+    return 0;
+}
+
