a3f238352453e0eb74a419792a426c040963278a
234992 <90908153+234992@users.noreply.github.com>
2024-06-03 15:37:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,17 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int main()
+{
+    long long a, b,c;
+    scanf("%lld %lld %lld", &a, &b,&c);
+    printf("%lld", a + b+c);
+   
+
+    return 0;
+}
+
+
+
+
+
+
